8b008757b1803a77d1981a80e145fe4f07d20932
Cameron Terry <cammy.wan.kenobi@gmail.com>
2015-08-09 20:59:37 +0100

    Added more structure to the single.php template.

--- single.php
+++ single.php
@@ -1,8 +1,22 @@
 <?php get_header(); ?>
-	<main role="main">
+	<main class="single clearfix" role="main">
 		<?php while ( have_posts() ) : the_post(); ?>
-			<h1><?php the_title(); ?></h1>
-			<?php the_content(); ?>
+			<article <?php post_class( 'article' ); ?>>
+				<h1><?php the_title(); ?></h1>
+				<p class="attribution">
+					Posted on
+					<time><?php the_date(); ?></time>
+					by
+					<?php the_author_posts_link(); ?>
+				</p>
+				<?php the_content(); ?>
+			</article>
+			<aside class="col">
+				Hello world
+			</aside>
+			<aside class="col">
+				Hello world
+			</aside>
 		<?php endwhile; ?>
 	</main>
 <?php get_footer(); ?>
