949cf29b45ec28f50479321eb4805ca6868c49cb
Cameron Terry <cammy.wan.kenobi@gmail.com>
2015-08-10 19:37:15 +0100

    Added additional image sizes.

--- functions.php
+++ functions.php
@@ -6,7 +6,12 @@ define( 'DZ_VERSION', '5.0.0' );
 require_once( DZ_INC . '/template-tags.php' );
 
 function dz_after_setup_theme() {
+	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'html5' );
+
+	add_image_size( 'album-small', 300, 300, true );
+	add_image_size( 'album-medium', 520, 520, true );
 }
 add_action( 'after_setup_theme', 'dz_after_setup_theme' );
 
