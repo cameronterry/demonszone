a7209ecf5ea1492e73bd70fa7495a802b8ff47cf
Cameron Terry <cammy.wan.kenobi@gmail.com>
2015-08-10 20:46:33 +0100

    Imported the old website template tags.

--- template-tags.php
+++ template-tags.php
@@ -1,5 +1,56 @@
 <?php
 
+function dz_album_artist() {
+    dz_the_term( 'artist' );
+}
+
+function dz_album_artist_permalink() {
+	dz_the_term( 'artist', '<a href="%2$s">%1$s</a>' );
+}
+
+function dz_album_rating( $prefix = 'Rating : ', $sep = ' / ' ) {
+	printf( '%3$s<span itemprop="ratingValue">%1$s</span>%2$s<span itemprop="bestRating">10</span>', get_field( 'rating' ), $sep, $prefix );
+}
+
+function dz_album_release_date( $format = null ) {
+	$release = DateTime::createFromFormat( 'Ymd', get_field( 'release_date' ) );
+	echo( $release->format( null === $format ? get_option( 'date_format' ) : $format ) );
+}
+
+function dz_get_pings() {
+    $comment_query = new WP_Comment_Query();
+    $comments = $comment_query->query( array(
+        'number' => 20,
+        'post_id' => get_the_ID(),
+        'type' => 'pings'
+    ) );
+
+    if ( empty( $comments ) ) {
+        return array();
+    }
+
+    return $comments;
+}
+
+function dz_the_slug_line() {
+    dz_the_term();
+}
+
+function dz_the_term( $taxonomy = 'category', $pattern = null ) {
+    $terms = get_the_terms( get_the_ID(), $taxonomy );
+    
+    if ( false === empty( $terms ) ) {
+        $term = current( $terms );
+
+        if ( empty( $pattern ) === false ) {
+            printf( '<a href="%2$s">%1$s</a>', $term->name, get_term_link( $term, $pattern ) );
+        }
+        else {
+            echo( $term->name );
+        }
+    }
+}
+
 /**
  * Takes the Artists associated with the Post and then
  * finds Albums.
