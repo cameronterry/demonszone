80987d4861c32fc9a0e556b424e2045feb2f2f25
Cameron Terry <cammy.wan.kenobi@gmail.com>
2015-08-10 21:12:57 +0100

    Added header for the Related Albums on the single.php template.

--- single.php
+++ single.php
@@ -14,6 +14,7 @@
 			<aside class="col">
 				<?php $related_albums = dz_get_related_albums(); ?>
 				<?php if ( $related_albums->have_posts() ) : ?>
+					<h2 class="heading">Related Albums:</h2>
 					<?php while ( $related_albums->have_posts() ) : $related_albums->the_post(); ?>
 						<article <?php post_class( array( 'related-article', 'image-overlay' ) ); ?>>
 							<a href="<?php the_permalink(); ?>">
