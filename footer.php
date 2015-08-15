31ab9f16997cbd33c487f1b2887c482314097c93
Cameron Terry <cammy.wan.kenobi@gmail.com>
2015-08-15 12:01:38 +0100

    Added a footer with the copyright notice.

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
 	</div>
+	<footer class="footer">
+		<p>Copyright DemonsZone <?php echo( date( 'Y' ) ); ?>, All Rights Reserved<span class="padding"></span><a href="mailto:hello@demonszone.com">hello@demonszone.com</a></p>
+	</footer>
 	<?php wp_footer(); ?>
 </body>
 </html>
