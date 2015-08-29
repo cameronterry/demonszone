a95e0ad77f3d8c238d14b0461eed7c47d96d28cf
Cameron Terry <cammy.wan.kenobi@gmail.com>
2015-08-29 21:26:59 +0100

    Changed the navigation to make the wp_nav_menu() output more presentable.

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
 		</div>
 	</div>
-	<div class="sb-slidebar sb-left sb-style-push">
-    	<p>Hello world</p>
-    </div>
+    <?php wp_nav_menu( array( 'container_class' => 'sb-slidebar sb-left sb-style-push main-navigation', 'menu' => 'primary' ) ); ?>
 	<footer class="footer">
 		<p>Copyright DemonsZone <?php echo( date( 'Y' ) ); ?>, All Rights Reserved<span class="padding"></span><a href="mailto:hello@demonszone.com">hello@demonszone.com</a></p>
 	</footer>
