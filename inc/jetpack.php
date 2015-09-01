d73e439d4871d5554c8543af129d947f04aecb2d
Cameron Terry <cammy.wan.kenobi@gmail.com>
2015-09-01 19:13:46 +0100

    Turned off the infinite scrolling for the home page.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -6,3 +6,8 @@ function dz_infinity_scroll() {
 		get_template_part( 'content', get_post_format() );
 	}
 }
+
+function dz_infinite_scroll_archive_supported() {
+	return ( !is_home() );
+}
+add_filter( 'infinite_scroll_archive_supported', 'dz_infinite_scroll_archive_supported' );
