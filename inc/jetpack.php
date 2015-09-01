8fe9063d8cd0b007233dc40e0d5456f8667576d2
Cameron Terry <cammy.wan.kenobi@gmail.com>
2015-09-01 20:03:44 +0100

    Disabled the Infinity Scroll on the single.php template (I thought this was a default?)

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -10,6 +10,10 @@ function dz_infinity_scroll() {
 }
 
 function dz_infinite_scroll_archive_supported() {
-	return ( !is_home() );
+	if ( is_home() || is_single() ) {
+		return false;
+	}
+
+	return true;
 }
 add_filter( 'infinite_scroll_archive_supported', 'dz_infinite_scroll_archive_supported' );
