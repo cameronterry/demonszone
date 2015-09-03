19a3526bc320e8b9120df873d75b7362b5a12a27
Cameron Terry <cammy.wan.kenobi@gmail.com>
2015-09-03 18:32:30 +0100

    Moved around some of the elements for the archive.php template.

--- archive.php
+++ archive.php
@@ -1,15 +1,19 @@
 <?php get_header(); ?>
 	<main class="main-content all-listing clearfix" role="main">
+		<h1><?php echo( single_term_title() ); ?></h1>
 		<?php while ( have_posts() ) : the_post(); ?>
-			<article <?php post_class( 'article' ); ?>>
-				<h2><?php the_title(); ?></h2>
+			<article <?php post_class( array( 'article', 'clearfix' ) ); ?>>
+				<h2>
+					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+				</h2>
 				<div>
 					<?php if ( has_post_thumbnail() ) : ?>
-						<div class="thumbnail"><?php the_post_thumbnail( 'news-thumb-tiny' ); ?></div>
+						<div class="thumbnail"><?php the_post_thumbnail( 'news-thumb' ); ?></div>
 					<?php endif; ?>
 					<?php the_excerpt(); ?>
+					<?php the_tags( '<p>', ', ', '</p>' ); ?>
 					<p class="attribution">
-						Posted on <time><?php the_date(); ?></time>
+						Posted on <time><?php echo( get_the_date() ); ?></time>
 						by <?php the_author_posts_link(); ?>
 					</p>
 				</div>
