db0c00bf3a0b32efbd0f0456cc9508c0341cce40
Cameron Terry <cammy.wan.kenobi@gmail.com>
2015-09-04 21:56:42 +0100

    And here is the first stab at the JavaScript for responsive embeds. Works for YouTube so far...

--- assets/js/dz.js
+++ assets/js/dz.js
@@ -1,5 +1,27 @@
 ( function ( $ ) {
+	
+	/** Responsive Embeds for the website. */
+	var response_embed = function () {
+		$( '.oembed-wrapper' ).each( function () {
+			var container_width = $( this ).outerWidth();
+
+			var $iframe = $( 'iframe', this );
+
+			var iframe_width = $iframe.outerWidth();
+			var iframe_height = $iframe.outerHeight();
+			var ratio = ( iframe_width / iframe_height );
+
+			$iframe.width( container_width );
+			$iframe.height( container_width / ratio );
+		} );
+	};
+
 	$( document ).ready( function() {
 		$.slidebars();
+		response_embed();
 	} );
+
+	/** Connect the responsive embed code to the resize event. */
+	$( window ).resize( response_embed );
+
 } )( jQuery );
