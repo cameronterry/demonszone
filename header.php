dd0773e7b90a999a16abc37a7db82f8d2cdb8faa
Cameron Terry <cammy.wan.kenobi@gmail.com>
2015-09-26 12:24:38 +0100

    Added the Cookie Consent box.

--- header.php
+++ header.php
@@ -21,6 +21,19 @@
         } )( jQuery );
 
     </script>
+
+    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+	<script type="text/javascript">
+	    window.cookieconsent_options = {
+	    	'message' : 'This website uses cookies to ensure you get the best experience on our website',
+	    	'dismiss' : 'Understood',
+	    	'learnMore' : 'More information',
+	    	'link' : '//demonszone.com/cookie-policy/',
+	    	'theme' : 'dark-floating'
+	    };
+	</script>
+	<script type="text/javascript" src="//s3.amazonaws.com/cc.silktide.com/cookieconsent.latest.min.js"></script>
+	<!-- End Cookie Consent plugin -->
 </head>
 <body <?php body_class(); ?>>
 	<?php do_action( 'body_open' ); ?>
