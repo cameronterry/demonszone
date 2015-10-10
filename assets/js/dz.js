21d5e8dea8131b1900e7e4d85543c573826331e2
Cameron Terry <cammy.wan.kenobi@gmail.com>
2015-10-10 10:56:43 +0100

    Added Social Interaction tracking for Google Analytics and Jetpack's Sharedaddy.

--- assets/js/dz.js
+++ assets/js/dz.js
@@ -16,13 +16,64 @@
 		} );
 	};
 
+	var dz_jetpack_ga_social = function () {
+		$( '.sharedaddy ul li a' ).each( function () {
+			/**
+			 * Hook up the hyperlink click button so we can track Social Interactions in
+			 * Google Analytics.
+			 */
+			var $sharedaddy_button = $( this );
+
+			$sharedaddy_button.click( function ( e ) {
+				var $this = $( this );
+				var classes = $this.attr( 'class' ).split( ' ' );
+				var social_network = '';
+
+				/**
+				 * Get the name of the Social Network from the list of classes. Jetpack's Sharedaddy
+				 * has a class which is prefixed with "share-" and it is always the first (for the
+				 * mode "Icon Only")
+				 */
+				if ( 0 < classes.length ) {
+					social_network = classes[0].replace( 'share-', '' ).replace( '-plus-1', '' );
+				}
+
+				if ( 'email' === social_network ) {
+					return;
+				}
+
+				/** Amend Press-This to be WordPress.com */
+				if ( 'press-this' === social_network ) {
+					social_network = 'wordpressdotcom';
+				}
+
+				/**
+				 * Handle the URL and get rid of any hash or querystring non-sense so we get a
+				 * clean URL for Google Analytics.
+				 */
+				var url = $sharedaddy_button.attr( 'href' )
+
+				if ( url ) {
+					url = url.split( /[?#]/ )[0];
+				}
+
+				/** Deep breath! Now(!!!) send the Social Integration to Google Analytics. */
+				if ( ga ) {
+					ga( 'send', 'social', social_network, 'share', url );
+				}
+			} );
+		} );
+	};
+
 	$( document ).ready( function() {
 		$.slidebars();
+
+		dz_jetpack_ga_social();
 		response_embed();
 
 		/** Jetpack Infinity Scroll logic. */
 		$( document.body ).on( 'post-load', function () {
-			console.log( jQuery( 'div[rel="advert"]' ) );
+			dz_jetpack_ga_social();
 	        dfp.cycle();
 	    } );
 	} );
