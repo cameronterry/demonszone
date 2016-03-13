4d105e8cde2bc56d5bdf7588f8c124bc79d2aad1
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-03-13 13:01:40 +0000

    Added the new sidebar.php template for the new article page.

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<aside id="secondary" class="sidebar widget-area" role="complementary">
+  <section class="widget widget_advert">
+    <h2 class="widget-title">Advert</h2>
+    <div rel="advert" data-sizes="300x250,300x600"></div>
+  </section>
+  <?php dynamic_sidebar( 'news-sidebar' ); ?>
+  <section class="widget widget_advert">
+    <h2 class="widget-title">Advert</h2>
+    <div rel="advert" data-sizes="300x250,300x600"></div>
+  </section>
+</aside>
