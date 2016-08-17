8b55cfa32efcb77f37f86673ebe6300efaf009e7
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-08-17 22:54:07 +0100

    Added the DZ_INC constant to the functions.php file.

--- src/functions.php
+++ src/functions.php
@@ -3,6 +3,8 @@
 define( 'DZ_VERSION', '6.0.0-rev1' );
 define( 'DZ_DB_VERSION', '28af254' );
 
+define( 'DZ_INC', get_stylesheet_directory() . '/inc/' );
+
 function dz_enqueue_styles() {
 	wp_enqueue_style( 'demonszone', get_stylesheet_uri(), array(), DZ_VERSION );
 }
