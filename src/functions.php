3aa13da785433ef9a1efe7a388e743ec56cbe4e7
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-08-17 22:50:49 +0100

    Added the basic version constants and the upgrade mechanism for the theme.

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,18 @@
 <?php
+
+define( 'DZ_VERSION', '6.0.0-rev1' );
+define( 'DZ_DB_VERSION', '28af254' );
+
+function dz_maybe_upgrade() {
+	$current_version = get_option( 'demonszone_version', null );
+
+	if ( DZ_VERSION !== $current_version ) {
+		update_option( 'demonszone_version', DZ_VERSION );
+	}
+
+	if ( DZ_DB_VERSION !== $current_version ) {
+		global $wpdb;
+		update_option( 'demonszone_db_version', DZ_DB_VERSION );
+	}
+}
+add_action( 'init', 'dz_maybe_upgrade' );
