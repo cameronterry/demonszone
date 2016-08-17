15612e42064dac9bd80c1536016f2930d7de25b4
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-08-17 22:32:17 +0100

    Got a skeleton for the gulpfile.js setup for later.

---
+++ build/gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require( 'gulp' ),
+	uglify = require( 'gulp-uglify' ),
+	rename = require( 'gulp-rename' );
+
+gulp.task( 'jscompress', function () {
+
+} );
+
+gulp.task( 'default', [
+	'jscompress'
+] );
