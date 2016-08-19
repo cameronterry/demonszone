c74daac683c48264ac26c16d845f5d317d4f7ec0
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-08-19 22:29:49 +0100

    Got the beginnings of the Gulp build process for JavaScript sorted out.

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,9 +1,22 @@
 var gulp = require( 'gulp' ),
+	concat = require( 'gulp-concat' ),
+	pump = require( 'pump' ),
 	uglify = require( 'gulp-uglify' ),
 	rename = require( 'gulp-rename' );
 
-gulp.task( 'jscompress', function () {
-
+gulp.task( 'jscompress', function ( callback ) {
+	pump( [
+		gulp.src( ['javascript/zepto.js'] ),
+		concat( 'dz.js' ),
+		gulp.dest( '../src/' ),
+		uglify( {
+			preserveComments : 'license'
+		} ),
+		rename( {
+			extname: '.min.js'
+		} ),
+		gulp.dest( '../src/' )
+	], callback );
 } );
 
 gulp.task( 'default', [
