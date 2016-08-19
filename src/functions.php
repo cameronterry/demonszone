b7439e94b18760214e6920366f16ae3e09470dfc
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-08-19 22:30:39 +0100

    Changed the enqueue styles to use the right hook.

--- src/functions.php
+++ src/functions.php
@@ -12,7 +12,7 @@ require_once( DZ_INC . '/template-tags.php' );
 function dz_enqueue_styles() {
 	wp_enqueue_style( 'demonszone', get_stylesheet_uri(), array(), DZ_VERSION );
 }
-add_action( 'wp_enqueue_scripts', 'dz_enqueue_styles' );
+add_action( 'wp_enqueue_styles', 'dz_enqueue_styles' );
 
 function dz_maybe_upgrade() {
 	$current_version = get_option( 'demonszone_version', null );
