a17d22111066776b7338ae3167756253eee5d870
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-08-19 19:23:26 +0100

    Throwing the reset switch!

--- search.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-	<main class="main-content all-listing clearfix" role="main">
-		<h1>Search Results : "<?php echo( $_GET['s'] ); ?>"</h1>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'listing', get_post_type() ); ?>
-		<?php endwhile; ?>
-		<div id="content"></div>
-	</main>
-<?php get_footer(); ?>
