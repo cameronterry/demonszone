4cefde7a46c76d074c33e5624ded4e493b5ec0dc
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-08-21 11:37:35 +0100

    Updated the gulp file to process the less files properly.

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,18 +1,20 @@
 var gulp = require( 'gulp' ),
 	concat = require( 'gulp-concat' ),
+	less = require( 'gulp-less' ),
 	pump = require( 'pump' ),
+	rename = require( 'gulp-rename' ),
 	uglify = require( 'gulp-uglify' ),
-	rename = require( 'gulp-rename' );
+	util = require( 'gulp-util' );
 
 var destination_path = '../src/';
 
 gulp.task( 'less', function ( callback ) {
 	pump( [
-		gulp.src( ['site.less'] ),
+		gulp.src( ['less/theme.less', 'less/site.less'] ),
+		less().on( 'error', util.log ),
 		concat( 'style.css' ),
-		less(),
 		gulp.dest( destination_path )
-	] );
+	], callback );
 } );
 
 gulp.task( 'javascript', function ( callback ) {
