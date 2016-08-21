38dd2832ff14ec777e4c13df4dba29fb66c25675
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-08-21 10:50:47 +0100

    Condensed styles and scripts together and worked out a simpler way of doing SCRIPT_DEBUG for full-fat CSS and JS.

--- src/functions.php
+++ src/functions.php
@@ -10,19 +10,12 @@ require_once( DZ_INC . '/shortcodes.php' );
 require_once( DZ_INC . '/template-tags.php' );
 
 function dz_enqueue_scripts() {
-	if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
-		wp_enqueue_script( 'dz-dev', get_template_directory_uri() . '/dz.js', array(), DZ_VERSION, false );
-	}
-	else {
-		wp_enqueue_script( 'dz', get_template_directory_uri() . '/dz.min.js', array(), DZ_VERSION, false );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'dz_enqueue_scripts' );
+	$is_min = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min' );
 
-function dz_enqueue_styles() {
+	wp_enqueue_script( 'dz', get_template_directory_uri() . "/dz{$is_min}.js", array(), DZ_VERSION, false );
 	wp_enqueue_style( 'demonszone', get_stylesheet_uri(), array(), DZ_VERSION );
 }
-add_action( 'wp_enqueue_styles', 'dz_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'dz_enqueue_scripts' );
 
 function dz_maybe_upgrade() {
 	$current_version = get_option( 'demonszone_version', null );
