86b07906225daa44d03c66c5549f3ab35c48dad2
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-08-23 19:57:09 +0100

    Put in the basic structure for the News article (Post) template, enough to get working with at least.

---
+++ src/single-post.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<article <?php post_class() ?>>
+			<h1><?php the_title(); ?></h1>
+
+			<?php if ( has_post_thumbnail() ) : ?>
+				<?php the_post_thumbnail( 'dz-news-feature' ); ?>
+			<?php endif; ?>
+
+			<?php the_content(); ?>
+
+			<?php if ( comments_open() ) : ?>
+				<?php // ?>
+			<?php endif; ?>
+		</article>
+	<?php endwhile; ?>
+<?php get_footer(); ?>
