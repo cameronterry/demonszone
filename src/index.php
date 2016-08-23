85c9d3e8cd3529d53d69e5b52edcaabf1140f8d8
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-08-23 19:34:44 +0100

    Added a permalink to the index.php template so its a bit easier to get to the article template.

--- src/index.php
+++ src/index.php
@@ -3,7 +3,9 @@
 <h2>News</h2>
 
 <?php while ( have_posts() ) : the_post(); ?>
-	<h3><?php the_title(); ?></h3>
+	<h3>
+		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	</h3>
 <?php endwhile; ?>
 
 <?php get_footer(); ?>
