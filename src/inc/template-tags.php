9cb75154a240e1a68e05a6ec6e6930c29d486b92
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-11-23 21:35:07 +0000

    Added a template tag for retrieving an album rating.

--- src/inc/template-tags.php
+++ src/inc/template-tags.php
@@ -1,1 +1,10 @@
 <?php
+
+function dz_the_rating() {
+	$rating = intval( get_field( 'rating' ) );
+	printf( '%1$s / 10', $rating );
+
+	if ( 10 === $rating ) {
+		echo( '<span class="ten dashicons-star-filled"></span>' );
+	}
+}
