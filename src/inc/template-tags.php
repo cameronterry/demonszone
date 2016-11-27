fedb7af157f0837b33583bffbe024dbf523637ef
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-11-27 09:33:38 +0000

    Removed the star from the 10/10 rating as I’m not convinced it actually looks good …

--- src/inc/template-tags.php
+++ src/inc/template-tags.php
@@ -3,8 +3,4 @@
 function dz_the_rating() {
 	$rating = intval( get_field( 'rating' ) );
 	printf( '%1$s / 10', $rating );
-
-	if ( 10 === $rating ) {
-		echo( '<span class="ten dashicons-star-filled"></span>' );
-	}
 }
