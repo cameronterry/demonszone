fe829e4c4d050eed97d6a711c21293a0189ae4d4
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-12-25 12:52:16 +0000

    Created a new template tag for spitting out a basic purchase links.

--- src/inc/template-tags.php
+++ src/inc/template-tags.php
@@ -8,6 +8,28 @@ function dz_the_entry_meta() {
 	);
 }
 
+function dz_purchase_get_format() {
+	$formats = get_sub_field_object( 'format' );
+	$formats = $formats['choices'];
+	return $formats[get_sub_field( 'format' )];
+}
+
+function dz_purchase_the_format() {
+	echo( dz_purchase_get_format() );
+}
+
+function dz_purchase_the_links() {
+	echo( '<p style="font-weight:bold;">Buy Now : ' );
+
+	while ( have_rows( 'purchasing' ) ) {
+		the_row();
+
+		printf( '<a href="%1$s" rel="nofollow" title="%3$s">%2$s</a>&nbsp;', get_sub_field( 'url' ), dz_purchase_get_format(), get_sub_field( 'description' ) );
+	}
+
+	echo( '</p>' );
+}
+
 function dz_the_rating() {
 	$rating = intval( get_field( 'rating' ) );
 	printf( '%1$s / 10', $rating );
