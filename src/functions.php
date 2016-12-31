1fb424c4b2d152896b6a429c401e5942389fd213
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-12-31 14:36:26 +0000

    Added the rectangle medium image for the news content.

--- src/functions.php
+++ src/functions.php
@@ -42,5 +42,6 @@ add_action( 'init', 'dz_maybe_upgrade' );
 function dz_setup() {
 	/** Set the various image sizes. */
 	add_image_size( 'dz-square-medium', 400, 400, true );
+	add_image_size( 'dz-rectangle-medium', 400, 225, true );
 }
 add_action( 'after_setup_theme', 'dz_setup' );
