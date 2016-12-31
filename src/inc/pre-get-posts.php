babf6f2792750ef70424b54f3c97bf9f44c9b63b
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-12-31 15:16:31 +0000

    Made the homepage display 12 posts.

--- src/inc/pre-get-posts.php
+++ src/inc/pre-get-posts.php
@@ -6,6 +6,10 @@ function dz_pre_get_posts( $query ) {
 		return;
 	}
 
+	if ( is_home() && $query->is_main_query() ) {
+		$query->set( 'posts_per_page', 12 );
+	}
+
 	if ( is_post_type_archive( 'albums' ) || $query->is_tax( 'genres' ) ) {
 		$query->set( 'posts_per_page', 15 );
 	}
