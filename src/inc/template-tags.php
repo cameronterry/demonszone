b72d0dabcd1d69524a5ad1e9f208c583af841224
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-12-31 18:19:58 +0000

    Added another template tag for Category names and moved dz_the_entry_meta() into alphabetical order.

--- src/inc/template-tags.php
+++ src/inc/template-tags.php
@@ -36,6 +36,22 @@ function dz_purchase_the_links() {
 	echo( '</p>' );
 }
 
+function dz_the_cat_name( $slug ) {
+	$cat = get_category_by_slug( $slug );
+
+	if ( false === empty( $cat ) ) {
+		printf( '<a href="%2$s">%1$s</a>', $cat->name, get_category_link( $cat->term_id ) );
+	}
+}
+
+function dz_the_entry_meta() {
+	printf( '<span class="byline"><span class="author vcard"><span class="screen-reader-text">%1$s </span> <a class="url fn n" href="%2$s">%3$s</a></span></span>',
+		_x( 'Author', 'Used before post author name.', 'twentysixteen' ),
+		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+		get_the_author()
+	);
+}
+
 function dz_the_rating() {
 	$rating = intval( get_field( 'rating' ) );
 	printf( '%1$s / 10', $rating );
