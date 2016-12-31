45b32e9e43098ee3522d16492fb967dac9b76549
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-12-31 14:35:56 +0000

    Added a placeholder image for the Post on the Cell template.

--- src/template-parts/cell-post.php
+++ src/template-parts/cell-post.php
@@ -1,6 +1,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'cell' ); ?>>
 	<a href="<?php the_permalink(); ?>">
 		<h2 class="site"><?php the_title(); ?></h2>
-		<?php the_post_thumbnail( 'dz-rectangle-medium' ); ?>
+		<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail( 'dz-rectangle-medium' ); ?>
+		<?php else : ?>
+			<img src="http://placehold.it/400x225" title="Placeholder" />
+		<?php endif; ?>
 	</a>
 </article>
