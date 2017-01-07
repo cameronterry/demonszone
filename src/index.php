00c257658cbd88b5359ed752167423837f56737a
Cameron Terry <cammy.wan.kenobi@gmail.com>
2017-01-07 10:39:53 +0000

    Got the first iteration of the new site homepage although I probably need to have a bit of a rethink.

---
+++ src/index.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+	<div id="primary" class="home grid-area">
+		<main id="main" class="site-main clear" role="main">
+			<header class="page-header">
+				<h2 class="page-title">Newest Updates:</h2>
+			</header><!-- .page-header -->
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'template-parts/cell', 'post' ); ?>
+			<?php endwhile; ?>
+		</main>
+
+		<?php $section_posts = dz_get_posts( 'tour-dates', 6 ); ?>
+		<main class="site-main clear">
+			<header class="page-header">
+				<h2 class="page-title">News - <?php dz_the_cat_name( 'tour-dates' ); ?> :</h2>
+			</header><!-- .page-header -->
+
+			<?php while ( $section_posts->have_posts() ) : $section_posts->the_post(); ?>
+				<?php get_template_part( 'template-parts/cell', 'post' ); ?>
+			<?php endwhile; ?>
+		</main>
+
+		<?php $section_posts = dz_get_gigs( 9 ); ?>
+		<main class="site-main clear">
+			<header class="page-header">
+				<h2 class="page-title">
+					<a href="<?php echo( get_post_type_archive_link( 'gigs' ) ); ?>">Gig Reviews</a> :
+				</h2>
+			</header><!-- .page-header -->
+
+			<?php while ( $section_posts->have_posts() ) : $section_posts->the_post(); ?>
+				<?php get_template_part( 'template-parts/cell', 'post' ); ?>
+			<?php endwhile; ?>
+		</main>
+
+		<?php $section_posts = dz_get_posts( 'releases', 6 ); ?>
+		<main class="site-main clear">
+			<header class="page-header">
+				<h2 class="page-title">News - <?php dz_the_cat_name( 'releases' ); ?> :</h2>
+			</header><!-- .page-header -->
+
+			<?php while ( $section_posts->have_posts() ) : $section_posts->the_post(); ?>
+				<?php get_template_part( 'template-parts/cell', 'post' ); ?>
+			<?php endwhile; ?>
+		</main>
+	</div>
+<?php get_footer(); ?>
