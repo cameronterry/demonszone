ab1a3de79749c85d60d27266bb37d84a24679228
Cameron Terry <cammy.wan.kenobi@gmail.com>
2017-01-07 10:37:22 +0000

    Removed the old embed stuff as that applied to the old theme. And added the improved Facebook embeds through their new oEmbed endpoint.

--- src/inc/embeds.php
+++ src/inc/embeds.php
@@ -1,11 +1,22 @@
 <?php
+defined( 'ABSPATH' ) or die();
 
-function dz_responsive_video_init() {
-	add_filter( 'wp_video_shortcode', 'dz_responsive_video_embed_html' );
-	add_filter( 'video_embed_html', 'dz_responsive_video_embed_html' );
-}
-add_action( 'after_setup_theme', 'dz_responsive_video_init', 99 );
+wp_oembed_add_provider( '#https?://www\.facebook\.com/video.php.*#', 'https://www.facebook.com/plugins/video/oembed.json/', true );
+wp_oembed_add_provider( '#https?://www\.facebook\.com/.*/videos/.*#i', 'https://www.facebook.com/plugins/video/oembed.json/', true );
+wp_oembed_add_provider( '#https?://www\.facebook\.com/.*/posts/.*#i', 'https://www.facebook.com/plugins/post/oembed.json/', true );
+wp_oembed_add_provider( '#https?://www\.facebook\.com/.*/photos/.*#i', 'https://www.facebook.com/plugins/post/oembed.json/', true );
+wp_oembed_add_provider( '#https?://www\.facebook\.com/.*/activity/.*#i', 'https://www.facebook.com/plugins/post/oembed.json/', true );
+wp_oembed_add_provider( '#https?://www\.facebook\.com/photo(s/|.php).*#i', 'https://www.facebook.com/plugins/post/oembed.json/', true );
+wp_oembed_add_provider( '#https?://www\.facebook\.com/permalink.php.*#i', 'https://www.facebook.com/plugins/post/oembed.json/', true );
+wp_oembed_add_provider( '#https?://www\.facebook\.com/media/.*#i', 'https://www.facebook.com/plugins/post/oembed.json/', true );
+wp_oembed_add_provider( '#https?://www\.facebook\.com/questions/.*#i', 'https://www.facebook.com/plugins/post/oembed.json/', true );
+wp_oembed_add_provider( '#https?://www\.facebook\.com/notes/.*#i', 'https://www.facebook.com/plugins/post/oembed.json/', true );
 
-function dz_responsive_video_embed_html( $html ) {
-	return sprintf( '<div class="video">%s</div>', $html );
+function dz_jetpack_embed_remove( $shortcode ) {
+	if ( array_key_exists( 'facebook', $shortcode ) ) {
+		unset( $shortcode['facebook'] );
+	}
+
+	return $shortcode;
 }
+add_filter( 'jetpack_shortcodes_to_include', 'dz_jetpack_embed_remove', 10, 1 );
