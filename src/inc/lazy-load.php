60cace3bb5203a383f5f87aa27956a5faf9bd984
Cameron Terry <cammy.wan.kenobi@gmail.com>
2017-01-08 17:18:18 +0000

    Fixed an issue where the lazy load was being applied to embeds and the Admin area.

--- src/inc/lazy-load.php
+++ src/inc/lazy-load.php
@@ -1,10 +1,10 @@
 <?php
 
 function dz_lazy_load_attr( $attr, $attachment, $size ) {
-	if ( false === array_key_exists( 'data-src', $attr ) ) {
+	if ( false === is_embed() && false === is_admin() && false === array_key_exists( 'data-src', $attr ) ) {
 		$attr['data-src'] = $attr['src'];
 		$attr['src'] = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAADhCAQAAACjWBeAAAAAEUlEQVR42mNk+M84ikbRIEYARr3hAWYOVV8AAAAASUVORK5CYII=';
-		
+
 		unset( $attr['srcset'] );
 	}
 
