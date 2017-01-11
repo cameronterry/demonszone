c1176e84541ae7d8dab2ac09cdb926e46835e05d
Cameron Terry <cammy.wan.kenobi@gmail.com>
2017-01-11 09:56:03 +0000

    Added pagination support and renamed by dz_get_posts() to dz_get_posts_by_cat().

--- src/inc/queries.php
+++ src/inc/queries.php
@@ -1,15 +1,17 @@
 <?php
 defined( 'ABSPATH' ) or die();
 
-function dz_get_gigs( $count ) {
+function dz_get_gigs( $count, $page = 1 ) {
 	return new WP_Query( array(
+		'paged' => $page,
 		'post_type' => 'gigs',
 		'posts_per_page' => $count
 	) );
 }
 
-function dz_get_posts( $slug, $count ) {
+function dz_get_posts_by_cat( $slug, $count, $page = 1 ) {
 	return new WP_Query( array(
+		'paged' => $page,
 		'category_name' => $slug,
 		'posts_per_page' => $count
 	) );
