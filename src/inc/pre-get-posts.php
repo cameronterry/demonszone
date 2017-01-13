4f62ad9383944b6f6b13b557838f928e3686cd94
Cameron Terry <cammy.wan.kenobi@gmail.com>
2017-01-13 20:19:08 +0000

    Main loop on the artists page is now limited to Posts and 8 of them.

--- src/inc/pre-get-posts.php
+++ src/inc/pre-get-posts.php
@@ -13,5 +13,10 @@ function dz_pre_get_posts( $query ) {
 	if ( is_post_type_archive( 'albums' ) || $query->is_tax( 'genres' ) ) {
 		$query->set( 'posts_per_page', 16 );
 	}
+
+	if ( $query->is_tax( 'artist' ) && $query->is_main_query() ) {
+		$query->set( 'post_type', 'post' );
+		$query->set( 'posts_per_page', 8 );
+	}
 }
 add_action( 'pre_get_posts', 'dz_pre_get_posts' );
