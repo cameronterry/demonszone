6d771cd0758cb4f326169a6a1b1c5b7e1195596c
Cameron Terry <cammy.wan.kenobi@gmail.com>
2017-01-13 20:18:47 +0000

    Now hide the Reviews part on the Artist page if there is no reviews for them.

--- src/taxonomy-artist.php
+++ src/taxonomy-artist.php
@@ -13,15 +13,17 @@
 			<?php endwhile; ?>
 		</main>
 
-		<?php $section_posts = dz_get_albums_by_artist( get_queried_object_id(), null, true ); ?>
-		<main class="site-main clear">
-			<header class="page-header">
-				<h2 class="page-title"><?php echo( single_term_title( '', false ) ); ?> - Reviews :</h2>
-			</header><!-- .page-header -->
+		<?php $albums = dz_get_albums_by_artist( get_queried_object_id(), null, true ); ?>
+		<?php if ( $albums->have_posts() ) : ?>
+			<main class="site-main clear">
+				<header class="page-header">
+					<h2 class="page-title"><?php echo( single_term_title( '', false ) ); ?> - Reviews :</h2>
+				</header><!-- .page-header -->
 
-			<?php while ( $section_posts->have_posts() ) : $section_posts->the_post(); ?>
-				<?php get_template_part( 'template-parts/cell', 'albums' ); ?>
-			<?php endwhile; ?>
-		</main>
+				<?php while ( $albums->have_posts() ) : $albums->the_post(); ?>
+					<?php get_template_part( 'template-parts/cell', 'albums' ); ?>
+				<?php endwhile; ?>
+			</main>
+		<?php endif; ?>
 	</div>
 <?php get_footer(); ?>
