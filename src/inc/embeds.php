479cf90a7bd48e812a64a2f5e3d1dee48dbf899f
Cameron Terry <cammy.wan.kenobi@gmail.com>
2017-02-04 17:46:29 +0000

    Do a declaration check on dz_jetpack_embed_remove() so that the preview of the new theme can work on the live DZ site.

--- src/inc/embeds.php
+++ src/inc/embeds.php
@@ -12,11 +12,13 @@ wp_oembed_add_provider( '#https?://www\.facebook\.com/media/.*#i', 'https://www.
 wp_oembed_add_provider( '#https?://www\.facebook\.com/questions/.*#i', 'https://www.facebook.com/plugins/post/oembed.json/', true );
 wp_oembed_add_provider( '#https?://www\.facebook\.com/notes/.*#i', 'https://www.facebook.com/plugins/post/oembed.json/', true );
 
-function dz_jetpack_embed_remove( $shortcode ) {
-	if ( array_key_exists( 'facebook', $shortcode ) ) {
-		unset( $shortcode['facebook'] );
-	}
+if ( false === function_exists( 'dz_jetpack_embed_remove' ) ) :
+	function dz_jetpack_embed_remove( $shortcode ) {
+		if ( array_key_exists( 'facebook', $shortcode ) ) {
+			unset( $shortcode['facebook'] );
+		}
 
-	return $shortcode;
-}
-add_filter( 'jetpack_shortcodes_to_include', 'dz_jetpack_embed_remove', 10, 1 );
+		return $shortcode;
+	}
+	add_filter( 'jetpack_shortcodes_to_include', 'dz_jetpack_embed_remove', 10, 1 );
+endif;
