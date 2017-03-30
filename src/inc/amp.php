e52ac46178a9f49e407be35807292158fc4403b9
Cameron Terry <cammy.wan.kenobi@gmail.com>
2017-03-30 14:48:44 +0100

    Added an AdSense advert to AMP to capitalise on any visits there.

--- src/inc/amp.php
+++ src/inc/amp.php
@@ -1,5 +1,11 @@
 <?php
 
+function dz_amp_head() { ?>
+    <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
+	<script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></script>
+<?php }
+add_action( 'amp_post_template_head', 'dz_amp_head' );
+
 /**
  * Google Analytics for AMP version of the websites.
  */
@@ -22,7 +28,29 @@
  <?php }
  add_action( 'amp_post_template_footer', 'dz_amp_add_analytics' );
 
-function dz_amp_google_analytics_head() { ?>
-        <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
-<?php }
-add_action('amp_post_template_head', 'dz_amp_google_analytics_head');
+function dz_amp_add_adsense() {
+	add_filter( 'the_content', 'dz_amp_add_adsense_slot' );
+}
+add_action( 'pre_amp_render_post', 'dz_amp_add_adsense' );
+
+function dz_amp_add_adsense_slot( $content ) {
+	$ad = '<amp-ad layout="fixed-height" height="250" type="adsense" data-ad-client="ca-pub-1085868034675667" data-ad-slot="2288749177"></amp-ad>';
+
+	$paragraphs = explode( '</p>', $content );
+
+	if ( 3 > count( $paragraphs ) ) {
+		return $content;
+	}
+
+	foreach ( $paragraphs as $index => $paragraph ) {
+		if ( trim( $paragraph ) ) {
+			$paragraphs[$index] .= $closing_p;
+		}
+
+		if ( 0 === $index ) {
+			$paragraphs[$index] .= $ad;
+		}
+	}
+
+	return implode( '', $paragraphs );
+}
