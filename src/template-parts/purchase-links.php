4f377fca9f63451c7bf275277a8d5637d4a9387f
Cameron Terry <cammy.wan.kenobi@gmail.com>
2017-04-07 08:40:15 +0100

    Created a brand new purchase links template part - for use on Albums embed and single templates.

---
+++ src/template-parts/purchase-links.php
@@ -0,0 +1,8 @@
+<h2>Where to buy :</h2>
+<ul class="purchase-links">
+	<?php while ( have_rows( 'purchasing' ) ) : the_row(); ?>
+		<li>
+			<?php printf( '<a href="%4$s" rel="nofollow">Click here to buy the "%1$s" version on <strong>%2$s</strong> through %3$s.</a>', get_sub_field( 'description' ), dz_purchase_get_format(), dz_purchase_get_store(), get_sub_field( 'url' ) ); ?>
+		</li>
+	<?php endwhile; ?>
+</ul>
