239d416ddde4e296a7e7386f56bb491f7a898548
Cameron Terry <cammy.wan.kenobi@gmail.com>
2017-04-14 22:45:43 +0100

    Added a new post action which provides an easy option to get the Shortlink for a post.

--- src/admin/columns.php
+++ src/admin/columns.php
@@ -1,6 +1,28 @@
 <?php
 defined( 'ABSPATH' ) or die();
 
+/**
+ * Post Row Actions
+ * ================
+ * Due to the new strategy of scheduling Facebook and Twitter posts, we now have
+ * a way on the Posts screen to quickly get the Shortlink. This is usually the
+ * wp.me URLs from Jetpack.
+ */
+function dz_post_row_actions( $actions, $post ) {
+	if ( dz_module_active( 'sharedaddy' ) ) {
+		$actions['shortlink'] = sprintf( '<a href="%1$s">Shortlink</a>', wp_get_shortlink( $post->ID ) );
+	}
+
+	return $actions;
+}
+
+/**
+ * Custom Columns
+ * ==============
+ * For the Albums custom post type, three additional columns are added to the
+ * Albums list screen; Release Date, Rating and Feature Image. These columns are
+ * essentially the mandatory fields for the Album reviews.
+ */
 function dz_columns_headers( $defaults ) {
 	return array_merge( $defaults, array(
 		'release_date' => 'Release Date',
@@ -43,16 +65,23 @@ function dz_columns_contents( $column_name, $post_id ) {
 }
 add_action( 'manage_albums_posts_custom_column', 'dz_columns_contents', 10, 2 );
 
+function dz_columns_contents_none( $title = '', $text = 'NONE' ) {
+	printf( '<strong style="color:#CC0000;" title="%2$s">%1$s</strong>', esc_html( $text ), esc_attr( $title ) );
+}
+
+/**
+ * Sortable columns
+ * ================
+ * The following is to make the custom columns Release Date and Rating custom
+ * fields. This allows an Editor to view the Highest Rated album reviews and to
+ * sort by Release Date.
+ */
 function dz_columns_sortable( $columns ) {
 	$columns['rating'] = 'dz_rating';
 	$columns['release_date'] = 'dz_release_date';
 	return $columns;
 }
 add_filter( 'manage_edit-albums_sortable_columns', 'dz_columns_sortable', 10, 1 );
-
-function dz_columns_contents_none( $title = '', $text = 'NONE' ) {
-	printf( '<strong style="color:#CC0000;" title="%2$s">%1$s</strong>', esc_html( $text ), esc_attr( $title ) );
-}
 
 function dz_admin_pre_get_posts( $query ) {
 	if ( is_admin() && $query->is_main_query() && ( $orderby = $query->get( 'orderby' ) ) ) {
