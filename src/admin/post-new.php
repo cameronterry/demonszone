1eafb296497673ccf8077230da06b0c834bc6ac1
Cameron Terry <cammy.wan.kenobi@gmail.com>
2018-03-19 20:58:31 +0000

    Fixed an issue where BST timezone was becoming an issue for future posts.

--- src/admin/post-new.php
+++ src/admin/post-new.php
@@ -10,12 +10,17 @@ function dz_auto_schedule_script() {
     $date = $wpdb->get_var("SELECT post_date FROM $wpdb->posts WHERE post_status = 'future' AND post_type IN ('post') ORDER BY post_date_gmt DESC LIMIT 1");
     
     $post_date = strtotime( $date );
-    
 ?>
     <script>
         ( function ( $ ) {
             $( document ).ready( function () {
+                /** Set the time to milliseconds. */
                 var last_post_date = new Date( <?php echo( ( $post_date * 1000 ) ); ?> );
+
+                /** Correct for timezone, which for BST moves an hour. */
+                last_post_date.setMinutes( last_post_date.getMinutes() + last_post_date.getTimezoneOffset() );
+
+                /** Set the time to last post date + 2 hours. */
                 last_post_date.setTime( last_post_date.getTime() + (2 * 60 * 60 * 1000 ) );
 
                 $( '#aa' ).val( last_post_date.getFullYear() );
